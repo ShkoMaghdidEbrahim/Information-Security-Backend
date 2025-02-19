0693d48ebf9e5d04400231211f5dabafe208a510
ShkoMaghdid <Shkoma.ranya@gmail.com>
2025-02-20 02:22:32 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_cors import CORS
+from routes.week_one import week_one_bp
+
+app = Flask(__name__)
+app.register_blueprint(week_one_bp)
+CORS(app)
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run()
---
+++ routes/week_one.py
@@ -0,0 +1,70 @@
+from flask import Blueprint, request, jsonify
+
+week_one_bp = Blueprint('week_one', __name__, url_prefix='/weekOne')
+
+
+@week_one_bp.route('/caesarEncrypt', methods=['GET', 'POST'])
+def caesar_encrypt():
+    data = request.get_json()
+    plaintext = data.get('plaintext', '')
+    shift = data.get('shift', 0)
+    encrypted_text = ""
+    for char in plaintext:
+        if char.isalpha():
+            shift_base = ord('A') if char.isupper() else ord('a')
+            encrypted_text += chr((ord(char) - shift_base + shift) % 26 + shift_base)
+        else:
+            encrypted_text += char
+    return jsonify({'encrypted_text': encrypted_text})
+
+
+@week_one_bp.route('/caesarDecrypt', methods=['GET', 'POST'])
+def caesar_decrypt():
+    data = request.get_json()
+    ciphertext = data.get('ciphertext', '')
+    shift = data.get('shift', 0)
+    plaintext = ""
+    for char in ciphertext:
+        if char.isalpha():
+            shift_amount = shift % 26
+            new_char = chr(((ord(char) - ord('A' if char.isupper() else 'a') - shift_amount) % 26) + ord(
+                'A' if char.isupper() else 'a'))
+            plaintext += new_char
+        else:
+            plaintext += char
+    return jsonify({'decrypted_text': plaintext})
+
+
+@week_one_bp.route('/caesarAttack', methods=['GET', 'POST'])
+def caesar_attack():
+    data = request.get_json()
+    ciphertext = data.get('ciphertext', '')
+    results = []
+    print(ciphertext)
+    for shift in range(26):
+        decrypted = ''
+        for char in ciphertext:
+            if char.isalpha():
+                base = ord('A') if char.isupper() else ord('a')
+                decrypted += chr((ord(char) - base - shift) % 26 + base)
+            else:
+                decrypted += char
+        results.append({
+            'shift': shift,
+            'decrypted_text': decrypted
+        })
+    return jsonify(results)
+
+
+@week_one_bp.route('/monoAlphabeticEncrypt', methods=['GET', 'POST'])
+def mono_alphabetic_encrypt():
+    data = request.get_json()
+    plaintext = data.get('plaintext', '')
+    shift = data.get('shift', 0)
+    encrypted_text = "".join(chr((ord(char) + shift) % 256) for char in plaintext)
+    return jsonify({'encrypted_text': encrypted_text})
+
+
+@week_one_bp.route('/monoAlphabeticDecrypt')
+def mono_alphabetic_decrypt():
+    return 'monoAlphabeticDecrypt'
